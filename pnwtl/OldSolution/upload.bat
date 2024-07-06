e67c579916758aa63cbbf813dce55298ba95cad5
BlazesRus (James) <BlazesRus@yahoo.com>
2024-07-06 17:30:11 -0400

    removed some old solution file stuff since not uploading to website

--- pnwtl/OldSolution/upload.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call hg update
-c:\utils\nant\bin\nant -D:"ver=%1" -D:"password=%2" upload %3 %4
--- pnwtl/OldSolution/uploadpypn.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call hg update
-c:\utils\nant\bin\nant -D:"ver=%1" -D:"password=%2" uploadpypn %3 %4
